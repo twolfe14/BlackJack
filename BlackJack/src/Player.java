ecaafdd49e9419bcffedcfaef044b2a1bb5086d1
twolfe14 <58575148+twolfe14@users.noreply.github.com>
2020-04-14 18:05:03 -0400

    Update Player.java

--- BlackJack/src/Player.java
+++ BlackJack/src/Player.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
 
 public class Player {
 
+	private ArrayList<Card> hand;
+	private boolean isDealer;
+	
+	public Player()
+	{
+		hand = new ArrayList<Card>();
+		isDealer = false;
+	}
+	
+	public void hit(Card c)
+	{
+		hand.add(c);
+	}
+	
+	public boolean isDealer()
+	{
+		return isDealer;
+	}
+	
+	public void setDealer(boolean d)
+	{
+		isDealer = d;
+	}
 }
