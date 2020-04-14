286eb7e8c0a181ef96614e9505f204ced7c5f17b
alainwelliver <48450580+alainwelliver@users.noreply.github.com>
2020-04-14 18:14:51 -0400

    Update GameRunner.java

--- BlackJack/src/GameRunner.java
+++ BlackJack/src/GameRunner.java
@@ -2,6 +2,11 @@
 public class GameRunner {
 
 	public static void main(String[] args) {
+		Player one = new Player();
+		Player two = new Player();
+		Player three = new Player();
+		Player four = new Player(); //dealer
+		four.setDealer();
 	}
 
 }
