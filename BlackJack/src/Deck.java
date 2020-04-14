a9f1b523bd96482821c959be7d4736f126453d37
twolfe14 <58575148+twolfe14@users.noreply.github.com>
2020-04-14 18:03:51 -0400

    Update Deck.java

--- BlackJack/src/Deck.java
+++ BlackJack/src/Deck.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
 
 public class Deck {
 
+	private ArrayList<Card> deck;
+	
+	public Deck()
+	{
+		for(int suit = 1 ; suit < 4 ; suit++)
+		{
+			for(int value = 1; value < 14; value++)
+				deck.add(new Card(suit, value));
+		}
+	}
 }
