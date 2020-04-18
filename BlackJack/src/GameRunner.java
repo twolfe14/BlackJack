da1f110f9431f94fbcb1e5b3397f16bd7fad9aab
twolfe14 <58575148+twolfe14@users.noreply.github.com>
2020-04-18 15:38:47 -0400

    Update GameRunner.java

--- BlackJack/src/GameRunner.java
+++ BlackJack/src/GameRunner.java
@@ -1,12 +1,30 @@
 
-public class GameRunner {
+import java.awt.*;
+import javax.swing.*;
 
+
+public class GameRunner extends JFrame{
+
+	GameRunner(){
+		setLayout(new FlowLayout());
+		
+		Card a = new Card(1, 'S');
+		Card b = new Card(1, 'H');
+		Card c = new Card(1, 'D');
+		Card d = new Card(1, 'C');
+	
+		setContentPane(a.getImage());
+	}
+	
 	public static void main(String[] args) {
-		Player one = new Player();
-		Player two = new Player();
-		Player three = new Player();
-		Player four = new Player(); //dealer
-		four.setDealer();
+		
+		GameRunner go = new GameRunner();
+
+		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		go.setVisible(true);
+		go.pack();
+		
+		
 	}
 
 }
