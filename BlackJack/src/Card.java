ef7ab1f26dd59a329b3f2cc257ad5a1d83c54b99
twolfe14 <58575148+twolfe14@users.noreply.github.com>
2020-04-18 15:38:03 -0400

    Update Card.java

--- BlackJack/src/Card.java
+++ BlackJack/src/Card.java
@@ -1,13 +1,38 @@
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 public class Card {
-	private int suit;
+	private char suit;
 	private int value;
-	private ImageIcon image;
+	private JLabel image;
 	
-	public Card(int s, int v)
+	public Card(int v, int s)
 	{
-		suit = s;
+		if(s == 1)
+			suit = 'H';
+		else if(s == 2)
+			suit = 'D';
+		else if(suit == 3)
+			suit = 'C';
+			else 
+			suit = 'S';
+			
+		
 		value = v;
+		if(value == 1)
+			image = new JLabel(new ImageIcon(getClass().getResource("A" + suit +".png")));
+		else if(value == 11)
+			image = new JLabel(new ImageIcon(getClass().getResource("J" + suit +".png")));
+		else if(value == 12)
+			image = new JLabel(new ImageIcon(getClass().getResource("Q" + suit +".png")));
+		else if(value == 13)
+			image = new JLabel(new ImageIcon(getClass().getResource("K" + suit +".png")));
+		else
+			image = new JLabel(new ImageIcon(getClass().getResource(""+ value + suit +".png")));
+	}
+	
+	public JLabel getImage()
+	{
+		return image;
 	}
 }
