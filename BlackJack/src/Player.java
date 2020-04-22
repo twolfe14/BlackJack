1ddb4c508cebcdb805cbc693439f8548d8317074
twolfe14 <58575148+twolfe14@users.noreply.github.com>
2020-04-21 22:07:11 -0400

    Update Player.java

--- BlackJack/src/Player.java
+++ BlackJack/src/Player.java
@@ -6,17 +6,31 @@ public class Player {
 	private ArrayList<Card> hand;
 	private boolean isDealer;
 	private Canvas cards;
+	private boolean pass;
+	private String name;
 	
-	public Player()
+	public Player(String n)
 	{
+		name = n;
 		hand = new ArrayList<Card>();
 		isDealer = false;
 		cards = new Canvas();
+		pass = false;
 	}
 	
 	public void hit(Card c)
 	{
 		hand.add(c);
+	}
+	
+	public boolean hasPassed()
+	{
+		return pass;
+	}
+	
+	public void pass()
+	{
+		pass = true;
 	}
 	
 	public boolean isDealer()
@@ -29,9 +43,19 @@ public class Player {
 		isDealer = d;
 	}
 	
+	public ArrayList<Card> getHand()
+	{
+		return hand;
+	}
+	
 	public Canvas getCards()
 	{
 		//redraw all the cards for every move
 		return cards;
+	}
+		
+	public String toString()
+	{
+		return name;
 	}
 }
